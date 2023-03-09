647f6a3ede3cf99d9de83676f3024a99b6659982
s1f102100793 <85666759+s1f102100793@users.noreply.github.com>
2023-03-09 15:59:01 +0900

    45 Reactでのイベントやstyleの扱い方を知る

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@
 import React from "react";
 
 const App = () => {
+  const onCLickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+    // 中身はcssのプロパティだから最後が','
+  };
   return (
     <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onCLickButton}>ボタン</button>
     </>
   );
   // JSのなかでreturnでhtmlタグを書いていくのがjsx記号
@@ -15,6 +22,7 @@ const App = () => {
   // エラー回避には<React.Fragment>がおすすめ
   // 一番楽なのは<></>で囲むこと
   // Reactでは表示の部分をコンポーネントとして分けて管理する。
+  // {{}}外側がjsを書きますというカッコで内側がjsのオブジェクトのカッコ
 };
 
 export default App;
